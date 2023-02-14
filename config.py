e366b211435ef452cc4f704098f16c5b1b186791
VVVor <122165711+VVVor@users.noreply.github.com>
2023-02-14 22:07:37 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-user_token = 'vk1.a.gyJUl1knDf0EmV8pxr7nKLVoezmouThwf6efALRuMwadAhgYO1T6Ui9KJ0_at1ZrUNGt4_6BZH8qS6NHg8gGJpBNJ-xnSeZmg_YEuG3gghKruR-aOAvBnTu7s4L6mdMpq7Ee3dlmjWtIKcdnve114KHIhH7kF6flzO1RoROhON6LNKMfwpU5Zm37g2pugJ3La0_G4ecPNV4E0lY8VhpN6w' # токен пользователя
-comm_token = 'vk1.a.Xleq5reKOUYZcpDsQoyGDa9F7CytVCOe9uxlIpggUHR7q1krl_vnD4Du5HC6n_pmoc1YuxfkwK-RSRZChWnbI-A-MvR-E5B2f9vE4ZLSseZxPUILumTY5MwZZcH8fu83AezsXHXgOnjaWpCWzi8IZi736-yj5gajuoxw5MyrpKWjwrw3SbAFarsrl1aoGFHkLmTFCxZ0nJoQwuXJMMN_5g' # токен сообщества
+user_token = '' # токен пользователя
+comm_token = '' # токен сообщества
 
 offset = 0 #сдвиг
 line = range(0, 1000) # последовательность для перебора найденных пользователей
 
-db_name = 'db_user'
-host = '127.0.0.1'
-user = 'postgres'
-password = '121211'
+db_name = ''
+host = ''
+user = ''
+password = ''
